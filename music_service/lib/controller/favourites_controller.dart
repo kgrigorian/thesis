229bc309f10847581daef672b4c9eba92a885f1a
Karen <karen.grigorjan@hashcoins.com>
2020-04-19 03:48:30 +0300

    favourites controller

--- music_service/lib/controller/favourites_controller.dart
+++ music_service/lib/controller/favourites_controller.dart
@@ -1,4 +1,5 @@
 import 'package:aqueduct/aqueduct.dart';
+import 'package:music_service/model/media_item.dart';
 import 'package:music_service/music_service.dart';
 
 class FavouritesController extends ResourceController {
@@ -7,7 +8,18 @@ class FavouritesController extends ResourceController {
   ManagedContext context;
 
   @Operation.post()
-  Future<Response> postForm() async {
-
+  Future<Response> toggleFavourite() async {
+    final Map<String, dynamic> body = await request.body.decode();
+    final existingMediaItemQuery = Query<MediaItem>(context)
+      ..where((m) => m.id).equalTo(body['id'] as int);
+    final existingMediaItem = await existingMediaItemQuery.fetchOne();
+    if (existingMediaItem !=null) {
+      final query = Query<MediaItem>(context)
+        ..values.favourite = !existingMediaItem.favourite
+        ..where((m) => m.id).equalTo(body['id'] as int);
+      await query.updateOne();
+      return Response.ok({});
+    }
+    return Response.notFound();
   }
 }
