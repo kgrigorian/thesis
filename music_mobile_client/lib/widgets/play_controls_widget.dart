150c6f75239be2496c67f8a6f904ccc4b713073a
Karen <karen.grigorian92@gmail.com>
2020-04-20 21:07:14 +0300

    main control refactoring

--- music_mobile_client/lib/widgets/play_controls_widget.dart
+++ music_mobile_client/lib/widgets/play_controls_widget.dart
@@ -1,6 +1,6 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
-import 'package:music_mobile_client/widgets/main_control_button_widget.dart';
+import 'package:music_mobile_client/main.dart';
 
 class PlayControls extends StatelessWidget {
   PlayControls(this.mediaItem);
@@ -25,7 +25,7 @@ class PlayControls extends StatelessWidget {
                   onPressed: mediaItem == queue.last
                       ? null
                       : AudioService.skipToPrevious),
-              MainControlButton(),
+              _MainControlButton(),
               IconButton(
                 icon: Icon(
                   Icons.fast_forward,
@@ -40,5 +40,52 @@ class PlayControls extends StatelessWidget {
         );
       },
     );
+  }
+}
+
+class _MainControlButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: AudioService.playbackStateStream,
+      builder: (context, state) {
+        final basicState = state?.data ?? BasicPlaybackState.none;
+        return GestureDetector(
+          child: Container(
+            decoration: BoxDecoration(
+                color: pinkColor, borderRadius: BorderRadius.circular(50.0)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                _getIconFromState(basicState),
+                size: 58.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          onTap: () {
+            if (basicState == BasicPlaybackState.playing) {
+              AudioService.pause();
+            }
+            if (basicState == BasicPlaybackState.paused) {
+              AudioService.play();
+            }
+            if (basicState == BasicPlaybackState.playing) {
+              AudioService.pause();
+            }
+          },
+        );
+      },
+    );
+  }
+
+  IconData _getIconFromState(basicState) {
+    if (basicState == BasicPlaybackState.playing) {
+      return Icons.pause;
+    }
+    if (basicState == BasicPlaybackState.paused) {
+      return Icons.play_arrow;
+    }
+    return Icons.stop;
   }
 }
